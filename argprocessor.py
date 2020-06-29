9caa895c29f6061d0cc1d2ccbb5282c9acb2afbf
VilleK <ville.keituri1@gmail.com>
2020-06-29 13:13:59 -0700

    processwindow progress

--- argprocessor.py
+++ argprocessor.py
@@ -13,7 +13,8 @@ class Resize_options:
 def process(root,filepath,imgpaths,outputpath,maxheight,maxwidth,preserveextensions=True,userextension=None,aspectratio=True,multithreading=False):
     print(f"MH:{maxheight} MW:{maxwidth}")
     master = root
-    args = Resize_options()
+    arg = Resize_options()
+    args = []
     args.multithreading = multithreading
     image_height = image_width = 0.0
     val_aspectratio = 0.0
@@ -21,18 +22,18 @@ def process(root,filepath,imgpaths,outputpath,maxheight,maxwidth,preserveextensi
         image = Image.open(imgpaths[i])
         image_width, image_height = image.size
         target_height       = target_width = 0
-        args.extension      = (userextension,image.format)[preserveextensions]
+        arg.extension      = (userextension,image.format)[preserveextensions]
         target_height       = get_targetsize(maxheight,image_height)
         target_width        = get_targetsize(maxwidth,image_width)
-        args.aspectratio = (target_width/target_height,image_width/image_height)[aspectratio]
+        arg.aspectratio = (target_width/target_height,image_width/image_height)[aspectratio]
         if(maxheight > 0):
             maxheight = (maxheight,target_height)[maxheight==target_height]
         if(maxwidth > 0):
             maxwidth = (maxwidth,target_width)[maxwidth==target_width]
         if(aspectratio):
             if(maxheight > 0 and maxwidth > 0):
-                tempwidth = target_height * args.aspectratio
-                tempheight = tempwidth / args.aspectratio
+                tempwidth = target_height * arg.aspectratio
+                tempheight = tempwidth / arg.aspectratio
                 if(tempwidth > target_width):
                     target_height = target_width / args.aspectratio
                     target_width = target_height * args.aspectratio
@@ -45,13 +46,16 @@ def process(root,filepath,imgpaths,outputpath,maxheight,maxwidth,preserveextensi
                 target_height = target_width / args.aspectratio
             else:
                 print(f"Can not calculate aspect ratio: maxheight:{maxheight} maxwidth:{maxwidth} target_height:{ target_height} target_width:{target_width}")
-        args.target_size = target_width,target_height
+        arg.target_size = target_width,target_height
+        args.append(arg)
+        """
         print(f"imagepath{imgpaths[i]}")
         print(f"originalsize{image.size}")
         print(f"targetsize :{args.target_size}")
         print(f"aspectratio:{args.aspectratio}")
         print(f"extension:{args.extension}\n")
-    process = pw.ProcessWindow(root=master,title="Is this working",filecount=10,width=650,height=200)
+        """
+    process = pw.ProcessWindow(root=master,None,None,None,False)
 #LEVEYS = KORKEUS * ASPECTRATIO
 def get_targetsize(xy_target,xy_image):
     target = 0;
--- processwindow.py
+++ processwindow.py
@@ -1,41 +1,72 @@
 import time
 import _thread 
 import tkinter as tk 
+import PIL
 from tkinter import * 
+import argprocessor
 
 class ProcessWindow(Frame):
-    def __init__(self,root,title,filecount,width,height,debug=False):
+    def __init__(self,root,args,filecount,filepaths,debug=False):
        #TODO REMOVE THIS PART
+        self.processing = False
+        self.filecount = filecount
+        self.filepaths = filepaths
+        self.args = args
         if(debug):
             self.processwindow = Frame.__init__(self,root)
             self.master = root 
         else:
             self.processwindow= tk.Toplevel(root)
-        self.width = width
-        self.height = height
-        self.filecount = filecount
+            #self.processwindow.geometry(str(self.width) + "x" + str(self.height))
         self.processedfiles = 0 
-        self.master.title("KURWA")
-        #self.processwindow.geometry(str(self.width) + "x" + str(self.height))
+        self.master.title("Converting images")
         self.createwindow()
-    
+
     def createwindow(self):
-        self.processframe = tk.Frame(master=self.processwindow,width=640,height=30,bg="blue")
-        self.processframe.grid(row=1,column=0,columnspan=10,rowspan=2)
+        self.processing = True
+        self.processframe = tk.Frame(master=self.processwindow,width=640,height=30)
+        self.processframe.grid(row=0,column=0,columnspan=10,rowspan=3)
         self.processindicators = []
+        self.label_process = Label(master=self.processframe,text="Converting images")
+        self.label_process.grid(row=0,column=3,columnspan=5)
+        self.label_processedimg = Label(master=self.processframe,text="SomeCurrentImage.jpg",font="TkDefaultFont 10 bold",relief=GROOVE)
+        self.label_processedimg.grid(row=2,column=0,columnspan=6,sticky="w")
+        self.btn_quit = Button(master=self.processframe, text="Cancel",command=self.cancelordone)
+        self.btn_quit.grid(row=2,column=7,columnspan=3,sticky="e",pady=5)
         for i in range(10):
-            #ind = tk.Canvas(master=self.processframe,border=1,relief=GROOVE,height=10,width=10,bg="red").grid(row=1,column=i)
-            self.processindicators.append(tk.Canvas(master=self.processframe,border=1,relief=GROOVE,height=20,width=20,bg="red"))
+            self.processindicators.append(tk.Canvas(master=self.processframe,border=3,relief=GROOVE,height=25,width=25,bg="red"))
             self.processindicators[i].grid(row=1,column=i)
         _thread.start_new_thread(self.threadtest,(self.processindicators,1,))
+        _thread.start_new_thread(self.conversionlblanimation,(self.label_process,))
+
+    def cancelordone(self):
+        if(self.processing):
+            exit()
+        else:
+            exit()#TODO do i need to cancel threads 
+    
+    def conversionlblanimation(self,lbl):
+        dotcount = 0
+        convtxt = "Converting images"
+        while(self.processing):
+            lbl.config(text=convtxt)
+            dotcount+=1
+            convtxt += "."
+            if(dotcount > 3):
+                convtxt = "Converting images"
+                dotcount = 0
+            time.sleep(1)
+
+    
     def threadtest(self,ind,sec):
         for j in range(len(ind)):
             time.sleep(sec)
             ind[j].config(bg="green")
+        self.processing = False
     
 
 
 if(__name__=="__main__"):
     top = Tk()
-    process = ProcessWindow(root=top,title="Is this working",filecount=10,width=650,height=200,debug=True)
+    process = ProcessWindow(top,None,10,None,debug=True)
     top.mainloop()
